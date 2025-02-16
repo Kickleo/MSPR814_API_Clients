634e54a06031d266844af572392bdd0bce112a04
leote <leotep34@gmail.com>
2025-02-16 21:22:36 +0100

    opened metrics

--- src/main/java/epsi/mspr814/kls/client/security/SecurityConfig.java
+++ src/main/java/epsi/mspr814/kls/client/security/SecurityConfig.java
@@ -47,6 +47,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/login").permitAll()
                         .requestMatchers("/api/clients/register").permitAll()
+                        .requestMatchers("/metrics").permitAll()
+                        .requestMatchers("/actuator/prometheus").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
